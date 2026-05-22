857da5c8cdcae4554af9c987429150a63ff161e2
Laura sthefanny <laurasthefanny50@gmail.com>
2026-05-22 16:43:09 -0300

    Add files via upload
    Java Exercise Three - Variables part 2
    
    Exercicio em Java Três - Variáveis parte 2

---
+++ ExThree.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ExThree {
+    public static void main (String args[]){
+        String hi;
+        hi = "Good Morning, ";
+
+        String name;
+        name = "User";
+
+        System.out.print(hi + name);
+    }
+}
