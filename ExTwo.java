fa2fdef002c845e0373494f0434e3ebbfd2f2a00
Laura sthefanny <laurasthefanny50@gmail.com>
2026-05-22 16:40:48 -0300

    Add files via upload
    Java Exercise Two - Variables part 1
    
    Exercicios em Java dois - Variáveis parte 1

---
+++ ExTwo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ExTwo{
+    public static void main (String args[]){
+      int i;
+      i = 7;
+
+      double x;
+      x = 3.5;
+
+      x = i;
+              System.out.print (x);
+    }
+  }
