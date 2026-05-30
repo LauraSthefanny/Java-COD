cfbef969ec8eeea2430934ca43e0633e430e91a4
Laura sthefanny <laurasthefanny50@gmail.com>
2026-05-29 22:17:24 -0300

    ExFour.java
    Java Exercise Four - Conditionals Part 1
    
    Exercicio quatro de Java - Condição parte 1

---
+++ ExFour.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ExFour {
+    public static void main (String args []){
+
+        boolean GitHub = false;
+
+        if(GitHub){
+            System.out.print ("This is my Git Hub");
+        } else {
+            System.out.print ("This is an test");
+        }
+    }
+}
